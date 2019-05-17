e55c992771669579250cae17f6ea1b7d09897d38
Bingo <21344290@qq.com>
2019-05-17 10:57:26 +0800

    add https

--- map3-edge/docker/registry/rootfs/bash/update-node-info.sh
+++ map3-edge/docker/registry/rootfs/bash/update-node-info.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-IP=`wget -q -O -  http://whatismyip.akamai.com/`
+ORI_IP=`wget -q -O -  http://whatismyip.akamai.com/`
 
-if ! wget -O /dev/null --tries 1 --timeout 1 http://$IP ; then
-  IP='127.0.0.1'
+if ! wget -O /dev/null --tries 1 --timeout 1 http://$ORI_IP ; then
+  ORI_IP='127.0.0.1'
 fi
 
+IP=`echo ${ORI_IP} | sed "s/\./-/g"`
 (cat <<EOF
 {
   "nodePublicIp": "113.66.217.71",
-  "tileServer": "http://113.66.217.71/v1/api/tile/{z}/{x}/{y}.pbf"
+  "tileServer": "https://113.66.217.71/v1/api/tile/{z}/{x}/{y}.pbf"
 }
 EOF
-) | sed -e "s,\(\"nodePublicIp\": \)\"[^\"]*\",\1\"$IP\",g" -e "s,\(http://\)[^/]*,\1$IP,g" > /tmp/node-info.json
+) | sed -e "s,\(\"nodePublicIp\": \)\"[^\"]*\",\1\"$ORI_IP\",g" -e "s,\(https://\)[^/]*,\1$IP.tile.map3.network,g" > /tmp/node-info.json
 cat /tmp/node-info.json > /usr/share/nginx/html/node-info.json
